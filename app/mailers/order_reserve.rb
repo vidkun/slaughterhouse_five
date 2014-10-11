90a83ad7e016277e851098ae696759b62b14e177
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-11 17:25:07 -0600

    update method name

--- app/mailers/order_reserve.rb
+++ app/mailers/order_reserve.rb
@@ -1,7 +1,7 @@
 class OrderReserve < ActionMailer::Base
   default from: "from@example.com"
 
-  def order_reserve(order)
+  def order_reserved(order)
     # @user = User
     # @order = Order
     mail(to: [order.user.email, 'garret@email.com'], subject: "Order has been received")
