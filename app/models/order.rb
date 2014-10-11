4e6c07fa8cc5e684ef61989d4d577c88ca22e9bd
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-11 12:08:52 -0600

    add validations to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,15 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :cow
+
+  validates :cut, :amount, presence: true
+  validates :amount, numericality: true
+  validate :amount_left
+
+  private
+
+  def amount_left
+    remaining = @cow.send("#{cut}_total".to_sym) - @cow.send("#{cut}_reserved".to_sym)
+    errors.add(:amount, 'not enough of this cut remaining') if amount > remaining
+  end
 end
