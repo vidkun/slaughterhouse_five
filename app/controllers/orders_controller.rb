543ab0129bdfc37a311dd25f63e6a3016f2f9646
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-11 11:56:02 -0600

    build out orders_controller with basic CRUD actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,69 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @orders = Order.all
+  end
+
+  def show
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def edit
+  end
+
+  def create
+    @order = Order.new(order_params)
+
+    respond_to do |format|
+      if @order.save
+        format.html { redirect_to orders_url, notice: "Order ##{@order.id} was successfully created." }
+        format.json { render :show, status: :created, location: @order }
+      else
+        format.html { render :new }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to orders_url, notice: "Order ##{@order.id} was successfully updated." }
+        format.json { render :show, status: :ok, location: @order }
+      else
+        format.html { render :edit }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    begin
+      @order.destroy
+      flash[:notice] = "Order ##{order.id} deleted!"
+    rescue StandardError => e
+      flash[:notice] = e.message
+    end
+    respond_to do |format|
+      format.html { redirect_to orders_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:cut,
+                                  :amount,
+                                  :user_id,
+                                  :cow_id)
+  end
 end
