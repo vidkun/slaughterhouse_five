89d3ff27473fef307973596c6dbdabe50d97a95b
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-11 10:56:44 -0600

    setup db and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141011165126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cows", force: true do |t|
+    t.decimal  "chuck_reserved",          precision: 8, scale: 2
+    t.decimal  "chuck_total",             precision: 8, scale: 2
+    t.decimal  "rib_reserved",            precision: 8, scale: 2
+    t.decimal  "rib_total",               precision: 8, scale: 2
+    t.decimal  "brisket_reserved",        precision: 8, scale: 2
+    t.decimal  "brisket_total",           precision: 8, scale: 2
+    t.decimal  "shank_reserved",          precision: 8, scale: 2
+    t.decimal  "shank_total",             precision: 8, scale: 2
+    t.decimal  "plate_reserved",          precision: 8, scale: 2
+    t.decimal  "plate_total",             precision: 8, scale: 2
+    t.decimal  "short_loin_reserved",     precision: 8, scale: 2
+    t.decimal  "short_loin_total",        precision: 8, scale: 2
+    t.decimal  "sirloin_reserved",        precision: 8, scale: 2
+    t.decimal  "sirloin_total",           precision: 8, scale: 2
+    t.decimal  "top_sirloin_reserved",    precision: 8, scale: 2
+    t.decimal  "top_sirloin_total",       precision: 8, scale: 2
+    t.decimal  "bottom_sirloin_reserved", precision: 8, scale: 2
+    t.decimal  "bottom_sirloin_total",    precision: 8, scale: 2
+    t.decimal  "tenderloin_reserved",     precision: 8, scale: 2
+    t.decimal  "tenderloin_total",        precision: 8, scale: 2
+    t.decimal  "round_reserved",          precision: 8, scale: 2
+    t.decimal  "round_total",             precision: 8, scale: 2
+    t.decimal  "flank_reserved",          precision: 8, scale: 2
+    t.decimal  "flank_total",             precision: 8, scale: 2
+    t.decimal  "tongue_reserved",         precision: 8, scale: 2
+    t.decimal  "tongue_total",            precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.text     "address"
+    t.integer  "user_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
